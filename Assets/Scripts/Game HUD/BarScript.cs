ffc274ef28cfdf9d5ed6471b2acab99d4d5dbf02
Ethan Anderson <et714492@dal.ca>
2024-09-12 15:03:37 -0300

    bar text in unity instead of image (still need to remove the text from the images)

--- Assets/Scripts/Game HUD/BarScript.cs
+++ Assets/Scripts/Game HUD/BarScript.cs
@@ -1,6 +1,8 @@
 // Script to control the angle/power/spin bar
 
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class BarScript : MonoBehaviour
 {
@@ -8,6 +10,7 @@ public class BarScript : MonoBehaviour
     public static BarScript Instance;
 
     [SerializeField] private SpriteRenderer spriteRenderer;
+    [SerializeField] private TMP_Text barText;
     [SerializeField] private Sprite angleBarLeft;
     [SerializeField] private Sprite angleBarRight;
     [SerializeField] private Sprite powerBar;
@@ -35,20 +38,25 @@ public class BarScript : MonoBehaviour
             case "angle":
                 spriteRenderer.enabled = true;
                 spriteRenderer.sprite = (isLeftside ? angleBarLeft : angleBarRight);
+                barText.text = "angle";
                 break;
             case "power":
                 spriteRenderer.enabled = true;
                 spriteRenderer.sprite = powerBar;
+                barText.text = "power";
                 break;
             case "spin":
                 spriteRenderer.enabled = true;
                 spriteRenderer.sprite = spinBar;
+                barText.text = "spin";
                 break;
             case "none":
                 spriteRenderer.enabled = false;
+                barText.text = "";
                 break;
             default:
                 spriteRenderer.enabled = false;
+                barText.text = "";
                 break;
         }
         return type;
